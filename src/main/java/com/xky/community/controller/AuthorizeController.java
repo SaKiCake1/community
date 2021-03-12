a030fd6961f11f6b704fb13439ee7f39a04b5933
xky <1400822425@qq.com>
2021-03-12 16:29:39 +0800

    add User entity and other pom.xml content

--- src/main/java/com/xky/community/controller/AuthorizeController.java
+++ src/main/java/com/xky/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.xky.community.controller;
 
 import com.xky.community.dto.AccessTokenDto;
 import com.xky.community.dto.GithubUser;
+import com.xky.community.mapper.UserMapper;
+import com.xky.community.model.User;
 import com.xky.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,11 +11,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+
+    @Autowired
+    private UserMapper userMapper;
 
     /*@Value("${github.client.id}")
     private String clientId;
@@ -24,7 +32,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String AuthorizeController(@RequestParam("code") String code,
-                                      @RequestParam("state") String state) {
+                                      @RequestParam("state") String state,
+                                      HttpServletRequest request) {
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id("Iv1.fde4a2e0572f3a08");
         accessTokenDto.setClient_secret("046ae3b8068b78764ca41d74093dd6debcfc3cd7");
@@ -33,8 +42,22 @@ public class AuthorizeController {
         accessTokenDto.setState(state);
         githubProvider.getAccessToken(accessTokenDto);
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user);
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtModified());
+            userMapper.insert(user);
+            //登录成功，写cookie和session
+            //再session中放入user对象
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        } else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
---
+++ src/main/java/com/xky/community/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.xky.community.mapper;
+
+import com.xky.community.model.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * 用户Mapper
+ */
+@Mapper
+public interface UserMapper {
+    @Select("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
